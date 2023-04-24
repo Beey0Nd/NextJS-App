5e344446dfd8052affa3a383ed6089af9c133a23
Nikolay <ya.sapunovops2.ru@list.ru>
2023-04-24 17:06:03 +0300

    second

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,33 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import getEventById from "../../fake-data";
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
+
 function EventDetailPage() {
-    return ( 
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if(!event) {
+        return <p>No event found!</p>
+    }
+
+    return (
+        <Fragment>
+            <EventSummary title={event.title}/>
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAll={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
 }
 
